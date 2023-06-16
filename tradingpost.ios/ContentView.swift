019eb44935d922137e4100d4cf071f7a77e244a9
nolanshepherd <nolanshepherd14@gmail.com>
2023-06-16 23:24:15 +0900

    Initial Commit

---
+++ tradingpost.ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tradingpost.ios
+//
+//  Created by Nolan Shepherd on 6/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tradingpost.ios/tradingpost_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tradingpost_iosApp.swift
+//  tradingpost.ios
+//
+//  Created by Nolan Shepherd on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct tradingpost_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
