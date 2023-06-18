e01b39e1590af7b46fa9d724061068d82fe10dab
nolanshepherd <nolanshepherd14@gmail.com>
2023-06-18 15:15:58 +0900

    add TradeItem class

---
+++ tradingpost.ios/TradeItem.swift
@@ -0,0 +1,37 @@
+//
+//  TradeItem.swift
+//  tradingpost.ios
+//
+//  Created by Nolan Shepherd on 6/18/23.
+//
+
+import Foundation
+import SwiftUI
+
+class TradeItem {
+    var name: String
+    var type: String
+    var price: Double
+    var imageData: Data?
+
+    init(name: String, type: String, price: Double, imageData: Data? = nil) {
+        self.name = name
+        self.type = type
+        self.price = price
+        self.imageData = imageData
+    }
+
+    func getImage() -> Image? {
+        guard let imageData = imageData else {
+            return nil
+        }
+        
+        if let uiImage = UIImage(data: imageData) {
+            return Image(uiImage: uiImage)
+        }
+        else {
+            return nil
+        }
+    }
+}
+
